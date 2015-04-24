d67f90b89114e10574d4eb288f0fdfe15063ecf2
Konr Larson <larsonkonr@gmail.com>
2015-04-24 15:37:46 -0600

    changed login to be username not email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: params[:session][:email_address])
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       session[:username] = user.username
