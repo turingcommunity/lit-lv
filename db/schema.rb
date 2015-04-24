c56260cd1a934231e899f8d2316f926a11b11fe6
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-04-24 15:29:44 -0600

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,14 @@ ActiveRecord::Schema.define(version: 20150424205350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.date     "expiration_date"
+    t.string   "created_by"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "start_date"
@@ -27,15 +35,6 @@ ActiveRecord::Schema.define(version: 20150424205350) do
     t.string   "location"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "announcements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.date     "expiration_date"
-    t.string   "created_by"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
   end
 
   create_table "users", force: :cascade do |t|
