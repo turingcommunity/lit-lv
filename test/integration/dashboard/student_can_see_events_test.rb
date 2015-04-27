19c6cc75558121cc4ce20382e4a25d06c91f0126
MLee21 <minnie.lee89@gmail.com>
2015-04-27 15:38:46 -0600

    fixing hound stuff.

--- test/integration/dashboard/student_can_see_events_test.rb
+++ test/integration/dashboard/student_can_see_events_test.rb
@@ -4,12 +4,12 @@ class User::NewEventsTest < ActionDispatch::IntegrationTest
   test 'a student can see a single event that is displayed' do 
     FactoryGirl.create(:user, :default)
     event = Event.create(start_date: "12-04-14",
-                         start_time: "1:00 PM",
-                         end_date: "12-05-14",
-                         end_time: "5:00 PM",
-                         name: "Museum Outing",
-                         description: "Museum of Nature and Science",
-                         location: "LV Museum")
+      start_time: "1:00 PM",
+      end_date: "12-05-14",
+      end_time: "5:00 PM",
+      name: "Museum Outing",
+      description: "Museum of Nature and Science",
+      location: "LV Museum")
     visit "/"
     fill_in "session[username]", with: "richard@turing.io"
     fill_in "session[password]", with: "password"
@@ -19,5 +19,5 @@ class User::NewEventsTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Museum of Nature and Science")
     assert page.has_content?("LV Museum")   
   end
-  
+
 end
